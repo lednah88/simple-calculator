9dcf6e7d073cd7867811652e080a7a4c3fe974af
Rixongile Manyobe <lednah97@gmail.com>
2020-04-23 17:40:45 +0200

    corrected specs

--- spec/simple_calculator_spec.js
+++ spec/simple_calculator_spec.js
@@ -1,24 +1,24 @@
 const { add, multiply } = require("../src/simple_calculator")
 describe("add", function() {
     it("should add numbers", function() {
+        expect(add(1, 2, 3, 4, 5)).toEqual(15)
+    })
+    it("should add numbers", function() {
         expect(add(1, 2)).toEqual(3)
     })
     it("should add numbers", function() {
         expect(add(-1, -1)).toEqual(-2)
     })
-    it("should add numbers", function() {
-        expect(add(1, 2, 3, 4, 5)).toEqual(15)
-    })
 
 })
 describe("multiply", function() {
     it("should multiply numbers", function() {
-        expect(multiply(1, 2)).toEqual(2)
-    })
-    it("should add numbers", function() {
-        expect(multiply(-1, 2)).toEqual(-2)
-    })
-    it("should add numbers", function() {
         expect(multiply(1, 2, 3, 4, 5)).toEqual(120)
+    })
+    it("should add numbers", function() {
+        expect(multiply(1, 3)).toEqual(3)
+    })
+    it("should add numbers", function() {
+        expect(multiply(-1, 3)).toEqual(-3)
     })
 })
