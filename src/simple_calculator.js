af751ff53d53a498c9e38fd52458243627d85caf
Rixongile Manyobe <lednah97@gmail.com>
2020-04-23 12:36:53 +0200

    added specs in spec file

--- src/simple_calculator.js
+++ src/simple_calculator.js
@@ -1,25 +1,18 @@
-// function add() {
-//     var sum = 0
-//     for (var i = 0; i < arguments.length; i++) {
-//         sum += arguments[i]
-//     }
-//     return sum
-// }
-
-
-
-
-
+function add() {
+    var sum = 0
+    for (var i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    return sum
+}
 
 
 function multiply() {
     var product = 1
     for (var i = 0; i < arguments.length; i++) {
         product *= arguments[i]
-
     }
     return product
 }
-console.log(multiply(2, 4))
 
-// module.exports = { add, multiply }
+module.exports = { add, multiply }
